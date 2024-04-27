c6fb672975723933882a7996ceb7c554c7cf474f
MichaelPoe44 <michaelcp387@gmail.com>
2024-04-26 21:13:15 -0500

    debugging

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from time import sleep
-from Hashing import Users
+from Passwords import Users
 
 #
 # from gpiozero import Servo
@@ -28,6 +28,7 @@ class Gui(Frame):
         #decides if the display should be cleared
         self.shouldClear = False
         self.creating_new_user = False
+        self.deleting_user = False
 
 
 
@@ -37,59 +38,53 @@ class Gui(Frame):
         self.pack(fill=BOTH,expand=1)
         self.display = Label(self,text="",font=("TextGyreAdventor", 45),height=1,width=9,anchor=E)
         self.display.grid(row=0,column=0,columnspan=4,sticky=E+W+N+S)
-        self.info = Text(self,bg="lightgrey",fg="black",state=NORMAL,yscrollcommand=Scrollbar,height=31)
-        self.info.grid(row=0,column=4,columnspan=1,rowspan=4)
+        self.info = Text(self,bg="lightgrey",fg="black",state=NORMAL,yscrollcommand=Scrollbar,height=43)
+        self.info.grid(row=0,column=4,columnspan=1,rowspan=5)
         self.info.grid_propagate(True)
         self.display.focus()
 
         
-        
-        button = Button(self,bg="gray",text="1",font=("TextGyreAdventor", 60),width=2,borderwidth=0,highlightthickness=0,activebackground="white",command=lambda:self.process("1")) 
+        button = Button(self,bg="gray",text="1",font=("TextGyreAdventor", 60),width=2,borderwidth=0,highlightthickness=0,border=1,activebackground="white",command=lambda:self.process("1")) 
         button.grid(row=1 ,column=0,sticky=N+S+E+W ) 
 
-    
-        button = Button(self,bg="gray",text="2",font=("TextGyreAdventor", 60),borderwidth=0,highlightthickness=0,activebackground="white",command=lambda:self.process("2")) 
+        button = Button(self,bg="gray",text="2",font=("TextGyreAdventor", 60),borderwidth=0,highlightthickness=0,border=1,activebackground="white",command=lambda:self.process("2")) 
         button.grid(row=1 ,column=1,sticky=N+S+E+W )
 
-        button = Button(self,bg="gray",text="3",font=("TextGyreAdventor", 60),borderwidth=0,highlightthickness=0,activebackground="white",command=lambda:self.process("3"))
+        button = Button(self,bg="gray",text="3",font=("TextGyreAdventor", 60),borderwidth=0,highlightthickness=0,border=1,activebackground="white",command=lambda:self.process("3"))
         button.grid(row=1 ,column=2,sticky=N+S+E+W)
 
-       
-        button = Button(self,bg="gray",text="4",font=("TextGyreAdventor", 60),borderwidth=0,highlightthickness=0,activebackground="white",command=lambda:self.process("4"))
+        button = Button(self,bg="gray",text="4",font=("TextGyreAdventor", 60),borderwidth=0,highlightthickness=0,border=1,activebackground="white",command=lambda:self.process("4"))
         button.grid(row=2 ,column=0,sticky=N+S+E+W)
 
-        button = Button(self,bg="gray",text="5",font=("TextGyreAdventor", 60),width=2,height=1,borderwidth=0,highlightthickness=0,activebackground="white",command=lambda:self.process("5"))
+        button = Button(self,bg="gray",text="5",font=("TextGyreAdventor", 60),width=2,height=1,borderwidth=0,border=1,highlightthickness=0,activebackground="white",command=lambda:self.process("5"))
         button.grid(row=2 ,column=1 ,sticky=N+S+E+W)
 
-        button = Button(self,bg="gray",text="6",font=("TextGyreAdventor", 60),borderwidth=0,highlightthickness=0,activebackground="white",command=lambda:self.process("6"))
+        button = Button(self,bg="gray",text="6",font=("TextGyreAdventor", 60),borderwidth=0,highlightthickness=0,border=1,activebackground="white",command=lambda:self.process("6"))
         button.grid(row=2 ,column=2,sticky=N+S+E+W )
 
-        button = Button(self,bg="gray",text="7",font=("TextGyreAdventor", 60),borderwidth=0,highlightthickness=0,activebackground="white",command=lambda:self.process("7"))
+        button = Button(self,bg="gray",text="7",font=("TextGyreAdventor", 60),borderwidth=0,highlightthickness=0,border=1,activebackground="white",command=lambda:self.process("7"))
         button.grid(row=3 ,column=0,sticky=N+S+E+W)
 
-        button = Button(self,bg="gray",text="8",font=("TextGyreAdventor", 60),borderwidth=0,highlightthickness=0,activebackground="white",command=lambda:self.process("8"))
+        button = Button(self,bg="gray",text="8",font=("TextGyreAdventor", 60),borderwidth=0,highlightthickness=0,border=1,activebackground="white",command=lambda:self.process("8"))
         button.grid(row=3 ,column=1,sticky=N+S+E+W )
 
-        
-        button = Button(self,bg="gray",text="9",font=("TextGyreAdventor", 60),borderwidth=0,width=2,height=1,highlightthickness=0,activebackground="white",command=lambda:self.process("9"))
+        button = Button(self,bg="gray",text="9",font=("TextGyreAdventor", 60),borderwidth=0,width=2,height=1,border=1,highlightthickness=0,activebackground="white",command=lambda:self.process("9"))
         button.grid(row=3 ,column=2,sticky=N+S+E+W )
 
-        #img = PhotoImage(file='images/bak.gif')
-        button = Button(self,bg="gray",text="<--",font=("TextGyreAdventor", 50),borderwidth=0,highlightthickness=0,activebackground="white",command=lambda:self.process("back"))
-       # button.image = img
+        button = Button(self,bg="gray",text="<--",font=("TextGyreAdventor", 50),borderwidth=0,highlightthickness=0,border=1,activebackground="white",command=lambda:self.process("back"))
         button.grid(row=1 ,column=3,sticky=N+S+E+W )
-
        
-        #img = PhotoImage(file='images/Lock.gif')
-        button = Button(self,bg="gray",text="Lock",font=("TextGyreAdventor", 50),borderwidth=0,highlightthickness=0,activebackground="white",command=lambda:self.process("Lock"))
-       # button.image = img
+        button = Button(self,bg="gray",text="Lock",font=("TextGyreAdventor", 50),borderwidth=0,highlightthickness=0,border=1,activebackground="white",command=lambda:self.process("Lock"))
         button.grid(row=2 ,column=3,sticky=N+S+E+W )
         
-        button = Button(self,bg="blue",text="Enter",font=("TextGyreAdventor", 50),borderwidth=0,highlightthickness=0,activebackground="white",command=lambda:self.process("Enter"))
-        button.grid(row=4,column=0,sticky=N+S+E+W,columnspan=2,ipady=30)
+        button = Button(self,bg="blue",text="Enter",font=("TextGyreAdventor", 40),borderwidth=0,highlightthickness=0,border=1,activebackground="white",command=lambda:self.process("Enter"))
+        button.grid(row=4,column=0,sticky=N+S+E+W,columnspan=3,ipady=30)
 
-        button = Button(self,bg="blue",text="Create\n Guest",font=("TextGyreAdventor", 20),borderwidth=0,highlightthickness=0,activebackground="white",command=lambda:self.process("new_user"))
+        button = Button(self,bg="gray",text="Create\n Guest",font=("TextGyreAdventor", 20),borderwidth=0,border=1,highlightthickness=0,activebackground="white",command=lambda:self.process("new_user"))
         button.grid(row=3,column=3,sticky=N+S+E+W)
+
+        button = Button(self,bg="grey",text="Remove\n Guest",font=("TextGyreAdventor", 20),borderwidth=0,border=1,highlightthickness=0,activebackground="white",command=lambda:self.process("del_user"))
+        button.grid(row=4,column=3,sticky=N+S+E+W)
 
     #for if access is granted
     def AccessGranted(self,name):
@@ -120,6 +115,26 @@ class Gui(Frame):
     def clear(self):
         self.display["text"] = ""
 
+    #removes a guest user
+    def delete(self,input:str):
+        self.deleted = "Guest"+input
+        del PASSWORDS.dict[self.deleted]
+        self.info.insert("1.0",f"{self.deleter} removed {self.deleted}\n")
+        self.deleting_user = False
+    
+
+    def confirm_pass(self, input):
+        temp = PASSWORDS.turn_hash(self.typed)
+        key = PASSWORDS.find(temp)
+        return key
+
+
+    def add_user(self,input:str):
+        PASSWORDS.add(input)
+        self.info.insert("1.0",f"Guest{PASSWORDS.counter-1} granted access by {self.creator}\n")
+        self.creating_new_user = False
+
+
 
 
     #processes the button pressed
@@ -137,8 +152,7 @@ class Gui(Frame):
 
 
         if buttonNum == "new_user":
-            temp = PASSWORDS.turn_hash(self.typed)
-            key = PASSWORDS.find(temp)
+            key = self.confirm_pass(self.typed)
             if key in PASSWORDS.admins:
                 self.creating_new_user = True
                 self.creator = key
@@ -149,7 +163,23 @@ class Gui(Frame):
                 self.display.config(background="red")
                 self.display["text"] = "Admins Only"
             self.shouldClear = True
-            
+        
+
+        if buttonNum == "del_user":
+            if len(PASSWORDS.dict)>3:
+                key = self.confirm_pass(self.typed)
+                if key in PASSWORDS.admins:
+                    self.deleting_user = True
+                    self.deleter = key
+                    self.display["text"] = "Delete which guest?"
+                else:
+                    self.display.config(background="red")
+                    self.display["text"] = "Admins Only"
+            else:
+                self.display.config(background="red")
+                self.display["text"] = "No Guests"
+            self.shouldClear = True
+
 
 
 
@@ -158,27 +188,44 @@ class Gui(Frame):
             #if not creating a new user
             #turns the current input on keypad into a hash and compares 
             #if it matches one in the stored users dictionary
-            if not self.creating_new_user:
-                temp = PASSWORDS.turn_hash(self.typed)
-                key = PASSWORDS.find(temp)
+            if not self.creating_new_user and not self.deleting_user:
+                #finds a users based on pass none if no users
+                key = self.confirm_pass(self.typed)  
                 if key:
                     self.AccessGranted(key)
                     print("unlocked")
                 else:
                     self.AccessDenied()
+
+            #will delete guest corresponding to that number
+            if self.deleting_user:
+                if len(self.typed)==1:
+                    self.delete(self.typed)
+                else:
+                    self.display.config(background="red")
+                    self.display["text"] = "Invalid Try Again"
+                self.clear()
+                
+
+
+
+
+
+
+
+
             #if creating a new user will create a guest user
             #but the pass must be 4 digits
             if self.creating_new_user:
                 if len(self.typed)==4:
-                    PASSWORDS.add(self.typed)
-                    self.info.insert("1.0",f"Guest{PASSWORDS.counter-1} granted access by {self.creator}\n")
-                    
+                    self.add_user(self.typed)
                     self.creating_new_user = False
                 else:
                     self.display.config(background="red")
-                    self.display["text"] = "Invalid Password"
-                    # self.creating_new_user = False
-
+                    self.display["text"] = "Invalid Try Again"
+                self.clear()
+                    
+            
             self.shouldClear = True
             
 
